ae5687db3a383c86e0a8f91b9f2b12bdeda99077
Sebastián Gaviria <vagyriag@hotmail.com>
2018-04-23 08:14:13 -0500

    view, insert, delete

--- index.js
+++ index.js
@@ -1,46 +1,74 @@
-const express = require('express'),
-      consolidate = require('consolidate'),
-      hbs = require('handlebars');
+const MongoClient = require('mongodb').MongoClient,
+    express = require('express'),
+    engines = require('consolidate'),
+    fs = require('fs');
 
-var app = express();
+var app = express(),
+    db;
 
-app.engine('hbs', consolidate.handlebars);
-app.set('view engine', 'hbs');
+app.engine('hbs', engines.handlebars);
+
 app.set('views', './views');
+app.set('view engine', 'hbs');
 
-app.use(express.static('public'));
+MongoClient.connect('mongodb://localhost:27017', function (err, client) {
+    if (err) throw err;
 
-// req: http request
-// res: http response
+    db = client.db('test');
+    var server = app.listen(1234);
+
+    /*db.collection('countries').ensureIndex('name.common', {
+        unique: true
+    });*/
+});
+
 app.get('/', function (req, res) {
-    res.render('index', {
-        titulo: 'Hola',
-        personas: [
-            'Sebastián',
-            'Sofía',
-            'Alberto'
-        ],
-        carros: [
-            {
-                nombre: 'Aveo',
-                llantas: 4,
-                cilindraje: 1.2,
-            },
-            {
-                nombre: 'Slingshot',
-                llantas: 3,
-                cilindraje: 1.4
+    db.collection('countries')
+        .find({
+            area: {
+                $gte: 47,
+                $lt: 91
             }
-        ]
-    });
+        }, {
+            projection: {
+                area: 1,
+                'name.common': 1,
+                _id: 0
+            },
+            limit: 100,
+            sort: {
+                area: 1
+            }
+        })
+        .toArray(function (err, result) {
+            res.render('countries', {
+                title: 'Paises con área mayor o igual a 47 y menor a 91',
+                countries: result
+            });
+        });
 });
 
-app.get('/personas', (req, res) => {
-    res.render('personas', {
-        texto: 'hola'
-    });
+app.get('/nuevo_pais', (req, res) => {
+    var pais = {
+        area: parseInt(req.query.area),
+        name: {
+            common: req.query.name
+        }
+    };
+    db.collection('countries')
+        .insertOne(pais, (err, result) => {
+            if(err){
+                res.send(err);
+            }else{
+                res.send(result);
+            }
+            //res.send(err ? err : result);
+        });
 });
 
-app.listen(3000, function(){
-    console.log('listo!');
+app.get('/eliminar_pais/:name', (req, res) => {
+    db.collection('countries')
+        .deleteMany({
+            'name.common': req.params.name
+        }, (err, result) => res.send(result));
 });
